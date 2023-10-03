a23a839c553e360c0298abacb609c423a8ee16f5
reymom <ramon_gs95@hotmail.com>
2023-10-03 16:09:50 +0200

    connect tauri commands to app execution

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,37 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![allow(unused)] // While exploring, remove for prod.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+pub use error::{Error, Result};
+
+use model::Store;
+use std::sync::Arc;
+
+mod error;
+mod ipc;
+mod macros;
+mod model;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let store = Store::new().await?;
+    let store = Arc::new(store);
+
+    tauri::Builder::default()
+        .manage(store)
+        .invoke_handler(tauri::generate_handler![
+            ipc::create_person,
+            ipc::get_person,
+            ipc::update_person,
+            ipc::delete_person,
+            ipc::list_persons,
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+
+    Ok(())
 }
 
 #[tauri::command]
 fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
+    format!("Hello, {}!", name)
 }
