5f846c34e6cb5802dfa7933a7cf9363822a4362d
reymom <ramon_gs95@hotmail.com>
2023-10-08 05:46:07 +0200

    ipc frontend

---
+++ src/api/api.ts
@@ -0,0 +1,46 @@
+import { ipc_invoke } from "./ipc";
+import { Person, PersonForCreate, PersonForUpdate } from "@/bindings";
+
+class Controller<M, C, U> {
+  suffix: string;
+
+  constructor(suffix: string) {
+    this.suffix = suffix;
+  }
+
+  async get(id: string): Promise<M> {
+    return ipc_invoke(`get_${this.suffix}`, { id }).then((res) => res.data);
+  }
+
+  async create(data: C): Promise<String> {
+    console.log("create data = ", data);
+    return ipc_invoke(`create_${this.suffix}`, { data }).then((res) => {
+      return res.data;
+    });
+  }
+
+  async update(id: string, data: U): Promise<String> {
+    return ipc_invoke(`update_${this.suffix}`, { id, data }).then((res) => {
+      return res.data;
+    });
+  }
+
+  async delete(id: string): Promise<String> {
+    return ipc_invoke(`delete_${this.suffix}`, { id }).then((res) => res.data);
+  }
+}
+
+class PersonController extends Controller<
+  Person,
+  PersonForCreate,
+  PersonForUpdate
+> {
+  constructor() {
+    super("person");
+  }
+
+  async list(): Promise<Person[]> {
+    return ipc_invoke(`list_${this.suffix}s`, {}).then((res) => res.data);
+  }
+}
+export const personController = new PersonController();
---
+++ src/api/ipc.ts
@@ -0,0 +1,40 @@
+/*
+ * Small wrapper on top of tauri api invoke
+ *
+ * We apply the idea of dependency injection to use a supplied invoke function as a
+ * function argument, rather than directly referencing the Tauri invoke function.
+ * Hence, don't import invoke globally in this file.
+ */
+
+import { invoke } from "@tauri-apps/api";
+
+export async function ipc_invoke(cmd: string, args?: object): Promise<any> {
+  //   const invoke = await import("@tauri-apps/api");
+
+  const response: any = await invoke(cmd, { args });
+
+  if (response.error != null) {
+    throw new Error(response.error);
+  } else {
+    return deepFreeze(response.result);
+  }
+}
+
+function deepFreeze<T>(obj: T): T {
+  if (Object.isFrozen(obj)) return obj;
+
+  // Retrieve the property names defined on object
+  const propNames = Object.getOwnPropertyNames(obj);
+
+  // Freeze properties before freezing self
+
+  for (const name of propNames) {
+    const value = (<any>obj)[name];
+
+    if (value != null && typeof value === "object") {
+      deepFreeze(value);
+    }
+  }
+
+  return Object.freeze(obj);
+}
